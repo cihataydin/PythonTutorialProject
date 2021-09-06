f82dc522a3bd4cf825c94efecb035b31e345fe5e
arif-aydin <aydinarif657@gmail.com>
2021-09-06 19:35:08 +0300

    initial commit

---
+++ 05_09_21.py
@@ -0,0 +1,13 @@
+print("arif kelimesi "+str(len("arif"))+" karakterdir")
+print("batuhan kelimesi "+str(len("batuhan"))+" karakterdir")
+print("osman kelimesi "+str(len("osman"))+" karakterdir")
+print("hüseyin kelimesi "+str(len("hüseyin"))+" karakteridir")
+print("ibrahim kelimesi "+str(len("ibrahim"))+" karakteridir")
+print("messi ve ronaldo kelimesi "+str(len("messi ve ronaldo"))+" karakteridir")
+
+b="arif"
+ilk =31
+harfi =43
+print(ilk , harfi, b, " kelimesinin ilk harfi ", b[0])
+arif = "62 "
+print(arif + b+" kelimesinin ilk harfi "+b[0])
---
+++ PrintFunction.py
@@ -0,0 +1,20 @@
+from typing import Mapping, MappingView
+
+
+for i in range(1, 10):
+    print(" "*(10 - i),"*"*(i*2-1))
+print("")
+
+for satir in range(1,10):
+    print(""*(10+satir), "*"*satir)
+
+
+print("örnek toplama işlemi 3 + 4 =", (3+4))
+print("örnek çıkarma işlemi 8 - 2 =", (8-2))
+print("örnek bölme işlemi   8 / 2 =", (8/2))
+print("örnek çarpma işlemi  5 * 6 =", (5*6))
+print("örnek kalan göstermeme (Bölümü gösterir) 22//7=",(22//7))
+print("örnek sadece kalan gösterme 18%4=",(18%4))
+bir="yazılım"
+son="bilimi"
+print(bir  +" "+ son)
