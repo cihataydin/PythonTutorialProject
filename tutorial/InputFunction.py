483e8c5690a7163c6100c9f8c077e0f7f2aa9912
cihataydin <cihataydin601@gmail.com>
2021-09-12 21:57:02 +0300

    test

--- tutorial/InputFunction.py
+++ tutorial/InputFunction.py
@@ -1,11 +1,8 @@
-a= input("lütfen isminizi giriniz:")
-b= input("lütfen soyadınızı giriniz:")
-c= input("lütfen e mail nizi giriniz:")
-d= input("lütfen tel no nuzu giriniz:")
- 
-bilgiler=[a,b,c,d]
+a = input("lütfen isminizi giriniz:")
+b = input("lütfen soyadınızı giriniz:")
+c = input("lütfen e mail nizi giriniz:")
+d = input("lütfen tel no nuzu giriniz:")
 
 print("bilgiler yükleniyor....")
-
-print("isminiz:",a,"soyadınız:",b,"e mailiniz:",c,"tel no nuz:",d,)
+print("isminiz:", a, "soyadınız:", b, "e mailiniz:", c, "tel no nuz:", d)
 print("bilgiler kaydedildi")
