6d3977afa03b90cdeedf26993f43b6b3765f73d8
arif-aydin <aydinarif657@gmail.com>
2021-09-17 13:57:03 +0300

    #2 el else konusu

---
+++ tutorial/el elf/elfFunction.py
@@ -0,0 +1,12 @@
+yas = int(input("Yaşınız kaç?:"))
+if  (yas>=18):
+        print("İçeri Girebilirsiniz Yaşınız uygundur Efendim...")
+elif(yas<=18):
+        print("İçeri Giremezsiniz! Yaşınız uygun değildir!!!")
+        
+
+
+
+
+
+
