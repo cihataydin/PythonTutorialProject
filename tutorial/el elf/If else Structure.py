ccdd09a19d1b4072691db7a6baa3dc1620d7736c
arif-aydin <aydinarif657@gmail.com>
2021-09-17 22:45:40 +0300

    #3 If else yapısı kuruldu.

--- tutorial/el elf/If else Structure.py
+++ tutorial/el elf/If else Structure.py
@@ -1,7 +1,19 @@
-sayı = (input("Sayı giriniz lütfen:"))
-if int(sayı)<0:
-    print("Bu sayı negatiftir")
-elif int(sayı)>0:
-    print("Bu sayı pozitiftir")
-else:
-    print("Bu sayı nötr'tür")   
+# Öncelikle kullanıcıdan değerleri aldık ve 
+# input string verdiği için integer a çevirdik.
+sayı = int(input("Sayı giriniz lütfen:"))
+sayı2= int(input("Sayı giriniz lütfen:"))
+
+# Toplam sonucunu bulduk.
+sonuc = sayı + sayı2
+
+# Sonuc değişkeninin değerini inceledik.
+if sonuc < 0:
+    print("Sonuç: {}. Bu sayı negatiftir.".format(sonuc))
+    #print("Sonuç: " + str(sonuc) + ". Bu sayı negatiftir.")
+    #print("Sonuç: ", str(sonuc), ". Bu sayı negatiftir.", sep="")
+elif sonuc > 0:
+    print("Sonuç: {}. Bu sayı pozitiftir.".format(sonuc))
+#elif sonuc == 0:
+#    print("Sonuc: {}. Bu sayı negatif veya pozitif değildir.".format(sonuc))
+else :
+    print("Sonuc: {}. Bu sayı negatif veya pozitif değildir.".format(sonuc))
