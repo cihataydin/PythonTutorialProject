4df21b125c81552216773dd743d85a1c7822ef59
arif-aydin <aydinarif657@gmail.com>
2021-09-19 16:59:36 +0300

    hesap makinesi analizi...

--- applications/calculator.py
+++ applications/calculator.py
@@ -2,12 +2,14 @@ print("Lütfen yapılıcak işlemi seçiniz:")
 print("-------------------------")
 print("1.toplama")
 print("2.çıkarma")
-secim = input("Seçiminiz iki şeyden oluşuyor...(1 ya da 2):")
 
+secim = input("Yapmak istediğiniz işlemin numarasını giriniz:")
 sayi1 = int(input("1. Sayı: "))
 sayi2 = int(input("2. Sayı: "))
 
+zed = sayi1 + sayi2
+
 if secim == "1":
-    print(sayi1,"+",sayi2,"işlem sonucunuz:", (sayi1+sayi2))
+    print( "işlem sonucunuz = {}".format(zed))
 elif secim == "2":
-    print(sayi1,"-",sayi2,"işlem sonucunuz:", (sayi1-sayi2))
+    print( "işlem sonucunuz =  {}".format(zed))
