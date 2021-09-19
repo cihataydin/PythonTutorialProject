3086e18b851316dae0ab8f0ceb98381f5e76f731
arif-aydin <aydinarif657@gmail.com>
2021-09-19 22:29:18 +0300

    #4 çarpma ve bölme eklendi hesap makinesine

--- applications/calculator.py
+++ applications/calculator.py
@@ -2,14 +2,27 @@ print("Lütfen yapılıcak işlemi seçiniz:")
 print("-------------------------")
 print("1.toplama")
 print("2.çıkarma")
+print("3.çarpma")
+print("4.bölme")
 
 secim = input("Yapmak istediğiniz işlemin numarasını giriniz:")
 sayi1 = int(input("1. Sayı: "))
 sayi2 = int(input("2. Sayı: "))
+ 
+if sayi2 == 0 and secim == "4":
+    print("girdiğiniz 2. sayı bölme işlemi için geçerli değildir.")
+    sayi2 = int(input("2. sayıyı tekrar giriniz:"))
 
-zed = sayi1 + sayi2
+toplama = sayi1 + sayi2
+çıkarma = sayi1 - sayi2
+çarpma = sayi1 * sayi2
+bölme = sayi1 / sayi2
 
 if secim == "1":
-    print( "işlem sonucunuz = {}".format(zed))
+    print( "işlem sonucunuz = {}".format(toplama))
 elif secim == "2":
-    print( "işlem sonucunuz =  {}".format(zed))
+    print( "işlem sonucunuz =  {}".format(çıkarma))
+elif secim =="3":
+    print ("işlem sonucunuz =  {}".format(çarpma))
+elif secim =="4":
+    print ("işlem sonucunuz =  {}".format(bölme))
