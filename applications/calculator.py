bcd7ea88a75e0483f52467eba0926b0efc3b76d5
arif-aydin <aydinarif657@gmail.com>
2021-09-20 23:01:53 +0300

    #4 hesap makinesi tamamlandı ve hatalar giderildi ..

--- applications/calculator.py
+++ applications/calculator.py
@@ -5,24 +5,34 @@ print("2.çıkarma")
 print("3.çarpma")
 print("4.bölme")
 
-secim = input("Yapmak istediğiniz işlemin numarasını giriniz:")
-sayi1 = int(input("1. Sayı: "))
-sayi2 = int(input("2. Sayı: "))
- 
-if sayi2 == 0 and secim == "4":
-    print("girdiğiniz 2. sayı bölme işlemi için geçerli değildir.")
-    sayi2 = int(input("2. sayıyı tekrar giriniz:"))
+while(True):
+    try:
+        secim = input("Yapmak istediğiniz işlemin numarasını giriniz:")
 
-toplama = sayi1 + sayi2
-çıkarma = sayi1 - sayi2
-çarpma = sayi1 * sayi2
-bölme = sayi1 / sayi2
+        if secim != "1" and secim != "2" and secim != "3" and secim != "4":
+            raise TypeError('Hatalı bir işlem kodu girdiniz')           
 
-if secim == "1":
-    print( "işlem sonucunuz = {}".format(toplama))
-elif secim == "2":
-    print( "işlem sonucunuz =  {}".format(çıkarma))
-elif secim =="3":
-    print ("işlem sonucunuz =  {}".format(çarpma))
-elif secim =="4":
-    print ("işlem sonucunuz =  {}".format(bölme))
+        sayi1 = int(input("1. Sayı: "))
+        sayi2 = int(input("2. Sayı: "))
+
+        if sayi2 == 0 and secim == "4":
+            print("girdiğiniz 2. sayı bölme işlemi için geçerli değildir.")
+            sayi2 = int(input("2. sayıyı tekrar giriniz:"))
+
+        toplama = sayi1 + sayi2
+        çıkarma = sayi1 - sayi2
+        çarpma = sayi1 * sayi2
+        bölme = sayi1 / sayi2
+
+        if secim == "1":
+            print( "işlem sonucunuz = {}".format(toplama))
+        elif secim == "2":
+            print( "işlem sonucunuz =  {}".format(çıkarma))
+        elif secim =="3":
+            print ("işlem sonucunuz =  {}".format(çarpma))
+        elif secim =="4":
+            print ("işlem sonucunuz =  {}".format(bölme))
+    except TypeError as error:
+        print(error)
+    except ValueError as error:
+        print("Girdiğiniz değer geçerli bir sayı değildir.")
